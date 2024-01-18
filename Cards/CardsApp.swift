41420c92f6648c32434940a833addb6537b8e4b5
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-18 14:33:15 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by GengYu Zhang on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
