f2209905efdd32eb80be2fa93f8a08af23afed9b
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-25 13:44:19 -0500

    Fininshed Prucduct

---
+++ Cards/View/PokemonListPlaceHolder.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonListPlaceHolder.swift
+//  Cards
+//
+//  Created by GengYu Zhang on 2024-01-25.
+//
+
+import SwiftUI
+
+struct PokemonListPlaceHolder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonListPlaceHolder()
+}
